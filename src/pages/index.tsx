ae5482462784051f63ab7b3bc07d57144379ef4e
k15015kk <aitk15015kk@outlook.com>
2023-08-22 02:16:14 +0900

    add: 選択フォームを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
-import { LineLayer, GeoJsonLayer, ColumnLayer } from '@deck.gl/layers/typed';
-import ComponentMap from '@/components/Map';
+import { GridCellLayer, GeoJsonLayer } from '@deck.gl/layers/typed';
 import { load } from '@loaders.gl/core';
 import { JSONLoader } from '@loaders.gl/json';
 import { useEffect, useState } from 'react';
+import DeckGL from '@deck.gl/react/typed';
+import Map from 'react-map-gl/maplibre';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
-// Viewport settings
+// 地図の一番最初に表示する場所を設定
 const INITIAL_VIEW_STATE: MapViewState = {
   longitude: 136.881637,
   latitude: 35.170694,
@@ -13,39 +15,60 @@ const INITIAL_VIEW_STATE: MapViewState = {
   bearing: 0
 };
 
-// Data to be used by the LineLayer
-const data = [
-  { sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781] }
+// 読み込みデータの定義
+const files: FileData[] = [
+  {
+    id: 1,
+    company: "JR東海",
+    line_file: './geojson/jrtokai_aikan_line.geojson',
+    station_file: './geojson/jrtokai_aikan_station_count.geojson',
+    color: [246, 170, 0]
+  },
+  {
+    id: 2,
+    company: "名古屋鉄道",
+    line_file: './geojson/meitetsu_line.geojson',
+    station_file: './geojson/meitetsu_station_count.geojson',
+    color: [255, 75, 0]
+  }
 ];
 
 export default function Home() {
 
+  function dataChangeHandler(select_value: string) {
+    const file_data = files.find(element => element.company == select_value);
+    setSelected(file_data ?? files[0]); 
+  }
+
   const [lineData, setLineData] = useState();
   const [stationData, setStationData] = useState();
 
+  const [selected, setSelected] = useState<FileData>(files[0]);
+
   useEffect(() => {
-    const loadLineData = async () => {
+    // 線形データを取得
+    async function loadLineData() {
       const line = await load(
-        './geojson/tokai_lines.geojson',
+        selected.line_file,
         JSONLoader
       );
 
       setLineData(line);
     }
 
-    const loadStationData = async () => {
+    // 駅データを取得
+    async function loadStationData() {
       const station = await load(
-        './geojson/tokai_station_counts.geojson',
+        selected.station_file,
         JSONLoader
       );
 
       setStationData(station.features);
     }
 
-    loadLineData()
-    loadStationData()
-  }, []);
-
+    loadLineData();
+    loadStationData();
+  }, [selected]);
 
   const lineGeoJsonLayer = new GeoJsonLayer({
     id: "line-geojson-layer",
@@ -55,33 +78,43 @@ export default function Home() {
     lineWidthScale: 100,
     lineWidthUnits: 'meters',
     lineWidthMinPixels: 2,
-    getLineColor: [237, 109, 0],
+    getLineColor: selected.color,
     getLineWidth: 8,
   })
 
-  const columnLayer = new ColumnLayer({
-    id: 'column-layer',
+  const customerGridCellLayer = new GridCellLayer({
+    id: 'customer-count-grid-cell-layer',
     data: stationData,
-    diskResolution: 12,
-    radius: 1000,
+    cellSize: 1000,
     extruded: true,
     pickable: true,
-    radiusUnits: 'meters',
-    elevationScale: 5,
-    getPosition: d => d.geometry.coordinates[0],
-    getFillColor: [237, 109, 0],
-    getElevation: d => d.properties.count
+    elevationScale: 10,
+    getPosition: d => d.geometry.coordinates,
+    getElevation: d => d.properties.count,
+    getFillColor: selected.color
   })
-
-  const layers = [
-    columnLayer,
-    lineGeoJsonLayer
-  ]
 
   return (
     <>
       <div className='h-screen'>
-        <ComponentMap viewState={INITIAL_VIEW_STATE} layers={layers} />
+        <DeckGL
+          initialViewState={INITIAL_VIEW_STATE}
+          layers={[customerGridCellLayer, lineGeoJsonLayer]}
+          controller={true}
+          style={{ position: 'relative', left: 'auto', top: 'auto' }}
+          getTooltip={({ object }) => object && `${object.properties.N05_011}\n${object.properties.count}`}
+        >
+          <Map
+            mapStyle={process.env.NEXT_PUBLIC_MAP_URL}
+          >
+          </Map>
+          <select
+            className='px-4 py-2 m-4 w-64 rounded shadow-md'
+            onChange={e => dataChangeHandler(e.target.value)}
+          >
+            {files.map((file) => <option key={file.id} value={file.company}>{file.company}</option>)}
+          </select>
+        </DeckGL>
       </div>
     </>
   )
---
+++ src/types/FileData.type.ts
@@ -0,0 +1,7 @@
+type FileData = {
+    id: number,
+    company: string,
+    line_file: string,
+    station_file: string,
+    color: number[]
+}
